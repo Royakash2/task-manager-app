d06bc99e0e949fd29bbeb7d633befe43213fdcbe
Royakash2 <yahmeakash@gmail.com>
2025-12-31 15:00:02 +0600

    feat: Implement Kinde-based user authentication and retrieval.

---
+++ app/data/user/get-user.ts
@@ -0,0 +1,15 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+
+export const userRequired = async () => {
+    const {isAuthenticated, getUser} = await getKindeServerSession()
+    const isUserAuthenticated = await isAuthenticated()
+    if (!isUserAuthenticated) {
+        throw new Error("Unauthorized")
+    }
+    const user = await getUser()
+    return {
+        user,
+        isUserAuthenticated
+    }
+}
+    
