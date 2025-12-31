13ec9c7d4316bfe8b79eeac2fa0bbdffaf262a8f
Royakash2 <yahmeakash@gmail.com>
2025-12-31 14:41:05 +0600

    feat: add Workspace page

---
+++ app/(protected)/workspace/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function WorkspacePage() {
+    return (
+        <div>
+            <h1 className='text-4xl font-bold'>Workspace page</h1>
+        </div>
+      
+    )
+}
