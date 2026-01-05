3779fbe174a362a9e0d9fd73e354316e92e294d0
Royakash2 <yahmeakash@gmail.com>
2026-01-05 19:30:47 +0600

    feat: add server action for user creation and onboarding flow.

--- app/actions/user.ts
+++ app/actions/user.ts
@@ -1,45 +1,52 @@
-'use server';
+"use server";
 
 import { userDatatype } from "@/components/OnboardingForm";
 import { userRequired } from "../data/user/get-user";
 import { userSchema } from "@/lib/schema";
 import db from "@/lib/db";
-
+import { redirect } from "next/navigation";
 
 export const createUser = async (data: userDatatype) => {
-    const { user } = await userRequired();
-    if (!user) {
-        throw new Error("Unauthorized: User not found");
-    }
-    const validateData = userSchema.safeParse(data);
+  const { user } = await userRequired();
+  if (!user) {
+    throw new Error("Unauthorized: User not found");
+  }
+  const validateData = userSchema.safeParse(data);
 
-    if (!validateData.success) {
-        throw new Error("Invalid data");
-    }
+  if (!validateData.success) {
+    throw new Error("Invalid data");
+  }
 
-    const userData = await db.user.create({
-        data: {
-            id: user.id,
-            email: data.email as string,
-            name: validateData.data.name,
-            about: validateData.data.about,
-            country: validateData.data.country,
-            indrustryType: validateData.data.industryType,
-            role: validateData.data.role,
-            onboardingCompleted: true,
-            image: data.image,
-            subscription: {
-               create: {
-                plan: "FREE",
-                status: "ACTIVE",
-                customerPeriodEnd: new Date(),
-                cancelAtPeriodEnd: false,
-                
-               }
-                
-            }
-            
-        }
-    })
-    return userData
+  const userData = await db.user.create({
+    data: {
+      id: user.id,
+      email: data.email as string,
+      name: validateData.data.name,
+      about: validateData.data.about,
+      country: validateData.data.country,
+      indrustryType: validateData.data.industryType,
+      role: validateData.data.role,
+      onboardingCompleted: true,
+      image: data.image,
+      subscription: {
+        create: {
+          plan: "FREE",
+          status: "ACTIVE",
+          customerPeriodEnd: new Date(),
+          cancelAtPeriodEnd: false,
+        },
+      },
+    },
+    select:{
+        id:true,
+        name:true,
+        email:true,
+        workspaces:true,
+    }
+  });
+//  Todo send user welcome email
+if(userData.workspaces.length === 0){
+   redirect("/create-workspace") 
 }
+redirect("/workspace")
+};
