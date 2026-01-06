44e0af0457be745445ddf57a2b3d06fed8f0e623
Royakash2 <yahmeakash@gmail.com>
2026-01-06 19:26:15 +0600

    feat: add protected workspace layout with sidebar, user workspace fetching, and redirection logic.

--- app/(protected)/workspace/[workspaceId]/layout.tsx
+++ app/(protected)/workspace/[workspaceId]/layout.tsx
@@ -1,14 +1,33 @@
+import { getUserWorkspaces } from '@/app/workspace/getUserWorkspace';
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { redirect } from 'next/navigation';
 import React from 'react'
 interface Props {
-    children: React.ReactNode;
-    params:Promise<{workspaceId: string}>;
+  children: React.ReactNode;
+  params: Promise<{ workspaceId: string }>;
 }
 
-const workspaceLayout = async ({children, params}: Props) => {
-    const {workspaceId} = await params;
-    console.log(workspaceId)
+const workspaceLayout = async ({ children, params }: Props) => {
+  const { data } = await getUserWorkspaces();
+  const { workspaceId } = await params;
+
+
+  if (data?.onboardingCompleted && !data?.workspaces) {
+    redirect("/create-workspace");
+  } else if (!data?.onboardingCompleted) {
+    redirect("/onboarding");
+  }
   return (
-    <div>workspaceId layout</div>
+    <SidebarProvider>
+     <div className='flex w-full bg-background h-screen'>
+      <AppSidebarContainer data={data} workspaceId={workspaceId}/>
+     </div>
+     <main className='w-full overflow-y-auto min-h-screen'>
+      <div className='flex items-start'>
+        <SidebarTrigger className='pt-3'/>
+      </div>
+     </main>
+    </SidebarProvider>
   )
 }
 
