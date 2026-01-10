afb0e6ea14f3332b688bcbf73720c26990bf9f03
Royakash2 <yahmeakash@gmail.com>
2026-01-10 23:47:12 +0600

    feat(workspace): implement role-based project fetching logic
    - Add Prisma imports and Enums for access levels
    - Implement conditional query construction:
      - Owners fetch all projects in the workspace
      - Members fetch only projects they have specific access to
    - Add db call to retrieve filtered project list

--- app/data/workspace/project/user-workspace-project.ts
+++ app/data/workspace/project/user-workspace-project.ts
@@ -1,27 +1,50 @@
 import db from "@/lib/db";
 import { userRequired } from "../../user/get-user";
+import { Prisma } from "@/lib/generated/prisma/client";
+import { $Enums } from "@prisma/client";
 
 export const getWorkspaceProjectByWorkspceId = async (workspaceId: string) => {
-    try {
-        const user = await userRequired();
-       const isUserMember = await db.workspaceMembers.findUnique({
-        where:{
-            userId_workspaceId:{
-                userId:user.user?.id as string,
-                workspaceId
-            }
-       }
-       });
-       if(!isUserMember){
-       throw new Error("you are not a member of this workspace");
-       }
-    } catch (error) {
-        console.log(error);
-        return{
-        success:false,
-        error:true,
-        message:"Failed to get workspace project"
+  try {
+    const user = await userRequired();
+    const isUserMember = await db.workspaceMembers.findUnique({
+      where: {
+        userId_workspaceId: {
+          userId: user.user?.id as string,
+          workspaceId,
+        },
+      },
+    });
+    if (!isUserMember) {
+      throw new Error("you are not a member of this workspace");
     }
-    }
-    
-}
+    const query: Prisma.ProjectWhereUniqueInput =
+      isUserMember.accessLevel === $Enums.AccessLevel.OWNER
+        ? { workspaceId }
+        : {
+            projectAccess: {
+              some: {
+                hasAccess: true,
+                workspaceMember: { userId: user?.user?.id, workspaceId },
+              },
+            },
+          };
+    const [projects, projectAccess] = await Promise.all([
+      db.project.findMany({
+        where: query,
+        select: {
+          id: true,
+          name: true,
+          description: true,
+          workspaceId: true,
+        },
+      }),
+    ]);
+  } catch (error) {
+    console.log(error);
+    return {
+      success: false,
+      error: true,
+      message: "Failed to get workspace project",
+    };
+  }
+};
