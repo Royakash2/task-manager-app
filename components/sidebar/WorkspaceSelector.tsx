331ae2fb6c4b34b9a218a988301c55c138ee0ee3
Royakash2 <yahmeakash@gmail.com>
2026-02-03 05:48:23 +0600

    feat: Add WorkspaceSelector component and useWorkSpaceId hook to manage workspace selection.

--- components/sidebar/WorkspaceSelector.tsx
+++ components/sidebar/WorkspaceSelector.tsx
@@ -1,12 +1,38 @@
 "use client"
+import { useWorkSpaceId } from '@/hooks/UseWorkspaceId';
 import { workspaceProps } from '@/utils/types'
-import React from 'react'
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react'
+import { SidebarMenu } from '../ui/sidebar';
 
 
-function WorkspaceSelector({workspaces}: workspaceProps) {
-  return (
-    <div>WorkspaceSelector</div>
-  )
+function WorkspaceSelector({ workspaces }: { workspaces: workspaceProps[] }) {
+
+    const router = useRouter();
+    const workSpaceId = useWorkSpaceId();
+    const [selectWorkspceId, setSelectWorkspce] = useState<workspaceProps | undefined>(undefined);
+
+    const onSlect = (id: string) => {
+        setSelectWorkspce(
+            workspaces.find((workspace) => workspace.id === id)
+        )
+        router.push(`/workspace/${id}`);
+    }
+    useEffect(() => {
+        if (workSpaceId && workspaces) {
+            setSelectWorkspce(
+                workspaces.find((workspace) => workspace.id === workSpaceId)
+            )
+        }
+    }, [workSpaceId, workspaces])
+
+    return (
+        <>
+            <SidebarMenu>
+
+            </SidebarMenu>
+        </>
+    )
 }
 
-export default WorkspaceSelector
+export default WorkspaceSelector;
