0dd1bdc54fc3234b24c412fc75a5e5639ef6eae2
Royakash2 <yahmeakash@gmail.com>
2026-02-04 08:09:56 +0600

    feat: add Radix UI-based DropdownMenu component and integrate it into a new WorkspaceSelector.

--- components/sidebar/WorkspaceSelector.tsx
+++ components/sidebar/WorkspaceSelector.tsx
@@ -3,7 +3,8 @@ import { useWorkSpaceId } from '@/hooks/UseWorkspaceId';
 import { workspaceProps } from '@/utils/types'
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react'
-import { SidebarMenu } from '../ui/sidebar';
+import { SidebarMenu, SidebarMenuSubItem } from '../ui/sidebar';
+import { DropdownMenu } from '../ui/dropdown-menu';
 
 
 function WorkspaceSelector({ workspaces }: { workspaces: workspaceProps[] }) {
@@ -29,7 +30,11 @@ function WorkspaceSelector({ workspaces }: { workspaces: workspaceProps[] }) {
     return (
         <>
             <SidebarMenu>
-
+                <SidebarMenuSubItem>
+                    <DropdownMenu>
+                        
+                    </DropdownMenu>
+                </SidebarMenuSubItem>
             </SidebarMenu>
         </>
     )
