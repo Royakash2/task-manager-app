fb6c92a7eabecac4d6d7c10464f50c74935e9195
Royakash2 <yahmeakash@gmail.com>
2026-02-15 08:59:36 +0600

    feat: create NavMain component to define sidebar navigation items and structure.

--- components/sidebar/NavMain.tsx
+++ components/sidebar/NavMain.tsx
@@ -1,10 +1,42 @@
 'use client'
 
 import { useWorkSpaceId } from "@/hooks/UseWorkspaceId"
-import { useSidebar} from "../ui/sidebar"
+import { SidebarGroup, SidebarGroupContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from "../ui/sidebar"
+import { CheckSquare, LayoutDashboard, Settings, Users } from "lucide-react"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 export const NavMain = () => {
-    const workspaceId = useWorkSpaceId() as string;
+    const workspaceId = useWorkSpaceId();
+    const { setOpenMobile } = useSidebar();
+
+    const items = [
+        {
+            label: "Home",
+            href: `/workspace/${workspaceId}`,
+            icon: LayoutDashboard,
+            path: "home",
+        },
+        {
+            label: "My Tasks",
+            href: `/workspace/${workspaceId}/my-tasks`,
+            icon: CheckSquare,
+            path: "my-tasks",
+        },
+        {
+            label: "Members",
+            href: `/workspace/${workspaceId}/members`,
+            icon: Users,
+            path: "members",
+        },
+        {
+            label: "Settings",
+            href: `/workspace/${workspaceId}/settings`,
+            icon: Settings,
+            path: "settings",
+        },
+    ]
+
     return (
 
         <>
