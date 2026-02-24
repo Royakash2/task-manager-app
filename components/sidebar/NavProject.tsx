065d193cbf355e1d37ea5199f5884f9dc518b19a
Royakash2 <yahmeakash@gmail.com>
2026-02-24 07:42:27 +0600

    feat: add `NavProject` component to display project navigation links in the sidebar.

--- components/sidebar/NavProject.tsx
+++ components/sidebar/NavProject.tsx
@@ -1,8 +1,9 @@
 'use-client'
 
 import { projectProps, workspaceMembersProps } from "@/utils/types"
-import { SidebarGroup, SidebarGroupLabel, useSidebar } from "../ui/sidebar"
+import { SidebarGroup, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from "../ui/sidebar"
 import { usePathname } from "next/navigation"
+import Link from "next/link"
 
 
 export const NavProject = ({
@@ -13,17 +14,37 @@ export const NavProject = ({
     ,
     workspaceMembers: workspaceMembersProps[]
 }) => {
-    const {isMobile,setOpenMobile} = useSidebar();
+    const { isMobile, setOpenMobile } = useSidebar();
     const pathName = usePathname();
     return (
         <>
-        <SidebarGroup className="group-data-[collapsible=icon]:hidden">
-            <SidebarGroupLabel className="flex justify-between">
-                <span className="text-sm font-semibold text-muted-foreground uppercase">
-                    Projects
-                </span>
-            </SidebarGroupLabel>
-        </SidebarGroup>
+            <SidebarGroup className="group-data-[collapsible=icon]:hidden">
+                <SidebarGroupLabel className="flex justify-between">
+                    <span className="text-sm font-semibold text-muted-foreground uppercase">
+                        Projects
+                    </span>
+                    {/* <CreateProject/> */}
+                    <SidebarGroupLabel>
+                        <SidebarMenu>
+                            {
+                                projects.map((project) => {
+                                    const href = `/workspace/${project.workspaceId}/project/${project.id}`;
+                                    // const isActive = pathName === href;
+                                    return (
+                                        <SidebarMenuItem key={project.id}>
+                                            <SidebarMenuButton>
+                                                <Link href={href} className={pathName === href ? "text-primary-foreground text-semibold" : "text-muted-foreground hover:text-primary-foreground"}>
+                                                    <span>{project.name}</span>
+                                                </Link>
+                                            </SidebarMenuButton>
+                                        </SidebarMenuItem>
+                                    )
+                                })
+                            }
+                        </SidebarMenu>
+                    </SidebarGroupLabel>
+                </SidebarGroupLabel>
+            </SidebarGroup>
         </>
     )
 }
