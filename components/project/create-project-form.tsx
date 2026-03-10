798d351d362b754579485c8b080d71826888b0a9
Royakash2 <yahmeakash@gmail.com>
2026-03-10 23:24:25 +0600

    feat: add `CreateProjectForm` component for creating new projects.

--- components/project/create-project-form.tsx
+++ components/project/create-project-form.tsx
@@ -12,7 +12,7 @@ import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/
 import { Input } from "../ui/input"
 import { Textarea } from "../ui/textarea"
 import { Button } from "../ui/button"
-import { Dialog } from "../ui/dialog"
+import { Dialog, DialogContent } from "../ui/dialog"
 import { DialogTrigger } from "@radix-ui/react-dialog"
 import { Plus } from "lucide-react"
 
@@ -45,6 +45,63 @@ export const CreateProjectForm = ({
                     <Plus/>
                 </Button>
             </DialogTrigger>
+            <DialogContent>
+                <Card className='w-full max-w-md'>
+                <CardHeader>
+                    <CardTitle className='text-2xl font-bold'>Create new Workspace</CardTitle>
+                    <CardDescription>
+                        Setup a workspace for you and your team
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(handleSubmit)} className='w-full max-w-md space-y-5'>
+                            <FormField
+                                control={form.control}
+                                name='name'
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Workspace Name</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder='Enter workspace name' {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+
+                            <FormField
+                                control={form.control}
+                                name='description'
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Bio</FormLabel>
+                                        <FormControl>
+                                            <Textarea
+                                                placeholder='What is this workspace for?'
+                                                className='resize-none'
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+
+                            <div className='flex items-center gap-3 w-full'>
+                                <Button type='button' variant="outline" className='flex-1' disabled={pending}>
+                                    cancel
+                                </Button>
+                                <Button type='submit' disabled={pending} className='flex-1'>
+                                    {pending ? "Creating..." : "Create Workspace"}
+                                </Button>
+                            </div>
+                        </form>
+                    </Form>
+                </CardContent>
+            </Card>
+                
+            </DialogContent>
          </Dialog>
 
         </>
