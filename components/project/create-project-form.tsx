1c0251af3228ba8df4060643e1da602ab4238bf4
Royakash2 <yahmeakash@gmail.com>
2026-03-10 22:53:52 +0600

    feat: Add Zod schemas for user, workspace, and project, and introduce a CreateProjectForm component.

--- components/project/create-project-form.tsx
+++ components/project/create-project-form.tsx
@@ -1,10 +1,13 @@
 'use client'
 
+import { projectSchema } from "@/lib/schema"
 import { workspaceMembersProps } from "@/utils/types"
+import z from "zod"
 
 interface Props {
     workspaceMembers: workspaceMembersProps[]
 }
+type projectDataType = z.infer<typeof projectSchema>
 export const CreateProjectForm = ({
     workspaceMembers
 }: Props) => {
