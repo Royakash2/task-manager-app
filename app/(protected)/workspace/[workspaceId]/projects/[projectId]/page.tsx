0e3f5963f37d216e04abbb24bce3adb6a4841c8d
Royakash2 <yahmeakash@gmail.com>
2026-03-21 23:01:13 +0600

    feat: add ProjectPage component with initial tab navigation using Radix UI Tabs.

--- app/(protected)/workspace/[workspaceId]/projects/[projectId]/page.tsx
+++ app/(protected)/workspace/[workspaceId]/projects/[projectId]/page.tsx
@@ -1,3 +1,5 @@
+import { Tabs } from '@/components/ui/tabs';
+import { TabsList } from '@radix-ui/react-tabs';
 import React from 'react'
 interface ProjectPageProps {
     params: Promise<{ workspaceId: string; projectId: string }>;
@@ -8,7 +10,14 @@ const ProjectPage = async (props: ProjectPageProps) => {
     const { workspaceId, projectId } = await props.params;
     const searchParams = await props.searchParams;
     return (
-        <div className='flex flex-col pb-3 px-3'>ProjectPage</div>
+        <div className='flex flex-col pb-3 px-3'>
+            <Tabs defaultValue={(searchParams.view as string) || 'Dashboard'}
+            className='w-full'>
+                <TabsList>
+                    
+                </TabsList>
+            </Tabs>
+        </div>
     )
 }
 export default ProjectPage;
