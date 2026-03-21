4c58929ee292230ec056368f07936f45ea327efe
Royakash2 <yahmeakash@gmail.com>
2026-03-21 22:59:29 +0600

    feat: create ProjectPage component for displaying individual project details.

--- app/(protected)/workspace/[workspaceId]/projects/[projectId]/page.tsx
+++ app/(protected)/workspace/[workspaceId]/projects/[projectId]/page.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+interface ProjectPageProps {
+    params: Promise<{ workspaceId: string; projectId: string }>;
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
 
-export default function ProjectPage() {
+const ProjectPage = async (props: ProjectPageProps) => {
+    const { workspaceId, projectId } = await props.params;
+    const searchParams = await props.searchParams;
     return (
-        <div>ProjectPage</div>
+        <div className='flex flex-col pb-3 px-3'>ProjectPage</div>
     )
 }
+export default ProjectPage;
