0ef05abb26b9d57f9fc71edfd273085166cc2861
Royakash2 <yahmeakash@gmail.com>
2026-04-01 22:11:26 +0600

    feat: add CreateTaskDialog component with form initialization and trigger button

--- components/task/create-task-dialog.tsx
+++ components/task/create-task-dialog.tsx
@@ -1,13 +1,49 @@
+'use client'
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { taskFormSchema } from "@/lib/schema";
 import { projectProps } from "@/utils/types";
 
-export const CreateTaskDialog = ({
-    project
-}: {
-    project: projectProps
-}) => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "../ui/button";
+import { useWorkSpaceId } from "@/hooks/UseWorkspaceId";
+
+type TaskFormValues = z.infer<typeof taskFormSchema>;
+
+type Props = {
+  project: projectProps;
+};
+
+export const CreateTaskDialog = ({ project }: Props) => {
+  const router = useRouter();
+  const [open, setOpen] = useState(false);
+  const workspaceId = useWorkSpaceId();
+  const [pending, setPending] = useState(false);
+
+  const form = useForm<TaskFormValues>({
+    resolver: zodResolver(taskFormSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      status: "TODO",
+      dueDate: new Date(),
+      startDate: new Date(),
+      priority: "MEDIUM",
+      attachments: [],
+    },
+  });
+
+  const handleOnSubmit = async (data: TaskFormValues) => {};
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Create Task</Button>
+      </DialogTrigger>
+    </Dialog>
+  );
+};
