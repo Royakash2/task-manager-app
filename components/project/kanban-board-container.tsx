a12ff0badaa42e740066793ef3fcccc2a34c02f5
Royakash2 <yahmeakash@gmail.com>
2026-04-20 23:59:09 +0600

    feat: add KanbanBoardContainer component to manage and display project tasks by status

--- components/project/kanban-board-container.tsx
+++ components/project/kanban-board-container.tsx
@@ -2,10 +2,9 @@
 import { $Enums } from "@/lib/generated/prisma/browser"
 import { Column, ProjectTaskProps } from "@/utils/types"
 import { useRouter } from "next/navigation"
-import { useEffect, useState } from "react"
+import { startTransition, useEffect, useState } from "react"
 
-
-const columnsTitle : Record<$Enums.TaskStatus, string> = {
+const columnsTitle: Record<$Enums.TaskStatus, string> = {
     TODO: "Todo",
     IN_PROGRESS: "In Progress",
     IN_REVIEW: "In Review",
@@ -19,15 +18,23 @@ export default function KanbanBoardContainer({ initialTasks }: { initialTasks: P
     const router = useRouter();
     // if (initialTasks.length === 0) return null;
 
-    const [columns, setColumns] = useState<Column[]>(() => {
-        return Object.entries(columnsTitle).map(([status, title]) => ({
-            id: status as $Enums.TaskStatus,
-            title,
-            tasks: initialTasks
-                .filter((task) => task.status === status)
-                .sort((a, b) => a.position - b.position),
-        }));
-    });
+    const [columns, setColumns] = useState<Column[]>([]);
+
+    useEffect(() => {
+        const initialColumns: Column[] = Object.entries(columnsTitle).map(
+            ([status, title]) => ({
+                id: status as $Enums.TaskStatus,
+                title,
+                tasks: initialTasks
+                    .filter((task) => task.status === status)
+                    .sort((a, b) => a.position - b.position),
+            })
+        );
+
+        startTransition(() => {
+            setColumns(initialColumns);
+        });
+    }, [initialTasks]);
 
     return (
         <div>
