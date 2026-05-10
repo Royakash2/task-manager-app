bc4fcf9119ffc8ada91fab0d0cf586f98ad0b657
Royakash2 <yahmeakash@gmail.com>
2026-05-10 23:43:26 +0600

    feat: create TaskDetails component to display task information with project and assignee details

--- components/task/task-details.tsx
+++ components/task/task-details.tsx
@@ -1,11 +1,12 @@
 import { projectProps } from '@/utils/types'
 import { Task, User } from '@prisma/client'
 import React from 'react'
-import { Card, CardHeader, CardTitle } from '../ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
 import { ProjectAvatar } from '../project/Project-avatar'
+import { ProfileAvatar } from '../profile-avatar'
 export interface TaskProps {
   task:Task & {
-  assigneTo: User,
+  assigneeTo: User,
   project: projectProps
   }
 }
@@ -13,7 +14,7 @@ export interface TaskProps {
 const TaskDetails = ({ task }: TaskProps) => {
   return (
     <Card>
-        <CardHeader className='flex flex-col md:flex-row justify-between '>
+        <CardHeader className='flex flex-col md:flex-row justify-between items-start md:items-end '>
           <div >
             <CardTitle >{task.title}</CardTitle>
             <div className='flex items-center gap-2 mt-3 '>
@@ -25,7 +26,28 @@ const TaskDetails = ({ task }: TaskProps) => {
             </div>
           </div>
 
+          <div className="w-full  md:w-auto flex flex-col justify-end items-end gap-2">
+            {/* <EditTaskDialog
+              key={new Date().getTime()}
+              task={task}
+              project={task.project}
+            /> */}
+
+            <div className="flex items-center gap-2">
+              <span className="text-sm text-muted-foreground">Assigned To:</span>
+              <ProfileAvatar
+                url={task.assigneeTo?.image || undefined}
+                name={task.assigneeTo?.name}
+              />
+              <span className="text-sm font-medium">{task.assigneeTo?.name}</span>
+            </div>
+          </div>
+
         </CardHeader>
+
+        <CardContent>
+
+        </CardContent>
     </Card>
 
   )
